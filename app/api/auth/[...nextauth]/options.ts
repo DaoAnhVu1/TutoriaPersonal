bd0fe77d52370aa9d98917407111f05ca8b6fe7c
DaoAnhVu <s3926187@rmit.edu.vn>
2023-11-28 22:03:38 +0700

    add code

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -8,7 +8,7 @@ export const options: AuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       profile(profile) {
-        console.log("Google Profile: " + profile);
+        console.log("Google Profile: " + JSON.stringify(profile));
 
         return {
           ...profile,
@@ -17,4 +17,20 @@ export const options: AuthOptions = {
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token = { ...user };
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      const currentDate = new Date();
+      const expires = new Date(currentDate);
+      expires.setDate(expires.getDate() + 1);
+      session.expires = expires.toISOString();
+      const toReturn = { ...session, id: token.sub };
+      return toReturn;
+    },
+  },
 };
