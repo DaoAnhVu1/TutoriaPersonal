89dc8c910ab17967b4f406c803add3ad38880e82
dbaonguyen <baond39@gmail.com>
2023-12-10 11:36:58 +0700

    update avatar for review user

--- components/tutorProfileComponents/display-review.tsx
+++ components/tutorProfileComponents/display-review.tsx
@@ -2,6 +2,7 @@
 import { Review } from "@prisma/client";
 import { FaStar } from "react-icons/fa";
 import { ScrollArea } from "../ui/scroll-area";
+import Image from "next/image";
 
 interface DisplayReviewProps {
   review: any[];
@@ -28,14 +29,21 @@ const DisplayReview = ({ review }: DisplayReviewProps) => {
         <>
           {review.map((reviewItem) => (
             <div
-              className="flex flex-col mt-5 border-2 shadow- rounded-md"
+              className="flex flex-col mt-5 border-2 shadow-md rounded-md"
               key={reviewItem.id}
             >
               <div className="ml-3 my-3">
-                <p className="font-semibold text-lg">
-                  {reviewItem.sender.name}
-                </p>
-                <StarRating rating={reviewItem.rating} />
+                <div className="flex flex-row">
+                  <div className="mr-2">
+                      <Image src={reviewItem.sender.imageUrl} alt="user image" className="rounded-full object-cover" width={50} height={50}/>
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="font-semibold text-lg">
+                      {reviewItem.sender.name}
+                    </p>
+                    <StarRating rating={reviewItem.rating} />
+                  </div>
+                </div>
                 <p className="mt-3">{reviewItem.description}</p>
               </div>
             </div>
