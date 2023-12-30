22c3abf417d10864bab620efbac56cd51b428400
dbaonguyen <baond39@gmail.com>
2023-12-31 00:59:49 +0700

    update validate available time

--- app/api/availabletimes/route.ts
+++ app/api/availabletimes/route.ts
@@ -3,7 +3,22 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   try {
     const { userId, startTime, endTime, weekDay } = await req.json();
-    console.log(weekDay);
+
+    // Check if the time slot already exists
+    const existingTimeSlot = await db.availableTime.findFirst({
+      where: {
+        userId: userId,
+        startTime: startTime,
+        endTime: endTime,
+        weekDay: Number.parseInt(weekDay),
+      },
+    });
+
+    if (existingTimeSlot) {
+      return new NextResponse("Time slot already exists", { status: 400 });
+    }
+
+    // If the time slot doesn't exist, create a new one
     const availableTime = await db.availableTime.create({
       data: {
         userId,
@@ -12,6 +27,7 @@ export async function POST(req: Request) {
         weekDay: Number.parseInt(weekDay),
       },
     });
+
     return NextResponse.json({ success: true }, { status: 200 });
   } catch (error) {
     console.log("[AVAILABLE TIMES ERROR]", error);
@@ -24,8 +40,8 @@ export async function DELETE(req: Request) {
     const { searchParams } = new URL(req.url);
     const availableTimeId = searchParams.get("availableTimeId")!;
     const userId = searchParams.get("userId")!;
-    console.log("Available Time: " + availableTimeId)
-    console.log("User Id: " + userId)
+    console.log("Available Time: " + availableTimeId);
+    console.log("User Id: " + userId);
     await db.availableTime.delete({
       where: {
         userId: userId,
